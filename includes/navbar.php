fb7615f1fc2aa5a70b106498b2321f32add883f0
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-11-29 01:38:10 +0100

    structure&Navbar

---
+++ includes/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="./images/sellsy-logo.svg" alt="Logo"></a>
+        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button> -->
+        <div class="collapse navbar-collapse text-black" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Resources</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'includes/header.php' ?>
+
+<!-- <main class="container">
+    <h1>Welcome to Selssy Clone</h1>
+    <p>This is a basic implementation using HTML, CSS, Bootstrap, and PHP.</p>
+</main> -->
+<!-- <?php include "includes/footer.php" ?> -->
