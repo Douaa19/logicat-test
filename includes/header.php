d34acb501d9025b70795750c65de3d620b9c0d70
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-11-30 00:03:05 +0100

    headerTexts

--- includes/header.php
+++ includes/header.php
@@ -13,13 +13,13 @@
 <body class="w-100">
     <?php include 'includes/navbar.php' ?>
     <section class="container px-10">
-        <div class="d-flex justify-content-between align-items-center gap-5">
+        <div class="d-flex justify-content-between align-items-center gap-4">
             <div class="header bg-transparent py-10 hero-grid-left w-100">
                 <div>
                     <h1>Simplify your sales and financial management</h1>
                     <p>Sellsy, the only solution that lets you manage your marketing, sales, invoicing and cash flow all in one place.</p>
                     <div class="rating">
-                      <a href="https://fr.trustpilot.com/review/sellsy.com" class="rating-link" aria-label="Read our reviews on Trustpilot">
+                        <a href="https://fr.trustpilot.com/review/sellsy.com" class="rating-link" aria-label="Read our reviews on Trustpilot">
                         <div class="star-rating">
                           <span class="star full"></span>
                           <span class="star full"></span>
@@ -28,10 +28,9 @@
                           <span class="star half"></span>
                         </div>
                         <span class="rating-number">4.4/5</span>
-                      </a>
-                      <p>
-                        Our customers recommend us with over 220 reviews on Trustpilot, Capterra, GetApp, SoftwareAdvice, Appvizer & Google.
-                      </p>
+                        </a>
+                        <p class="">Our customers recommend us with over 220 reviews on <a href="https://www.trustpilot.com/review/sellsy.com" target="_blank" class="link-nostyle">Trustpilot,</a> <a href="https://www.capterra.com/p/133104/Sellsy/" target="_blank" class="link-nostyle">Capterra</a>, <a href="https://www.getapp.com/operations-management-software/a/sellsy/" target="_blank" class="link-nostyle">GetApp</a>, <a href="https://www.softwareadvice.com/crm/sellsy-profile/" target="_blank" class="link-nostyle">SoftwareAdvice</a>, <a href="https://www.appvizer.fr/relation-client/customer-relationship-management-crm/sellsy" target="_blank" class="link-nostyle">Appvizer</a> &amp; <a href="https://www.google.com/search?q=sellsy&oq=sellsy&aqs=chrome..69i57j46i131i175i199i433i512j69i60l6.1523j0j1&sourceid=chrome&ie=UTF-8#lrd=0x480153ec3df897c3:0xfd99b2e464867243,1,,," target="_blank" class="link-nostyle">Google.</a></p>
+
                     </div>
                 </div>
                 <div class="bg-transparent button">
@@ -39,7 +38,7 @@
                 </div>
             </div>
             <div class="hero-img bg-transparent d-flex justify-content-center align-items-center mx-5 w-100">
-                <img src="./images/img.png" alt="hero" class="">
+                <img src="./images/img.png" alt="hero" class="" width="85%">
             </div>
         </div>
         <div></div>
