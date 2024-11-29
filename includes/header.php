1ca4b9545947af041ce7804fb6bccabb607e6cbf
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-11-29 03:38:58 +0100

    animationButton

--- includes/header.php
+++ includes/header.php
@@ -9,5 +9,5 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <title>Sellsy Clone</title>
 </head>
-<body>
+<body class="container">
     <?php include 'includes/navbar.php' ?>
