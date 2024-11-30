7726711771bd4d4a36f1afd9dc20bf253c886b4d
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-11-30 02:49:14 +0100

    mainSection

--- index.php
+++ index.php
@@ -1,6 +1,46 @@
 <?php include 'includes/header.php' ?>
 
 <main class="container">
-    
+    <div class="section section-homepage">
+        <div class="container">
+            <div class="centered-content narrow">
+                <div class="">
+                    <h2>The CRM suite you'll love to use.</h2>
+                </div>
+            </div>
+        </div>
+        <div class="container layout">
+            <div class="layout-grid">
+                <img src="https://cdn.prod.website-files.com/6058a411307dccf59c654f37/6527ff05e44f9cb0778ad076_HOMEPAGE-ILLU-MARKETING.webp" loading="lazy" id="w-node-fca9af45-2664-e690-88f6-336e9c357568-4b021557" width="480" alt="Interface de Sellsy Marketing" srcset="https://cdn.prod.website-files.com/6058a411307dccf59c654f37/6527ff05e44f9cb0778ad076_HOMEPAGE-ILLU-MARKETING-p-500.webp 500w, https://cdn.prod.website-files.com/6058a411307dccf59c654f37/6527ff05e44f9cb0778ad076_HOMEPAGE-ILLU-MARKETING-p-800.webp 800w, https://cdn.prod.website-files.com/6058a411307dccf59c654f37/6527ff05e44f9cb0778ad076_HOMEPAGE-ILLU-MARKETING-p-1080.webp 1080w, https://cdn.prod.website-files.com/6058a411307dccf59c654f37/6527ff05e44f9cb0778ad076_HOMEPAGE-ILLU-MARKETING.webp 1200w" sizes="(max-width: 479px) 100vw, (max-width: 767px) 81vw, 480px" class="split-screen-homepage" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+                <div class="text-wrapper">
+                <div id="w-node-fca9af45-2664-e690-88f6-336e9c35756a-4b021557" class="homepage-product-name">
+                    <div class="lottie-picto-produit" data-w-id="fca9af45-2664-e690-88f6-336e9c35756b" data-animation-type="lottie" data-src="https://cdn.prod.website-files.com/6058a411307dccf59c654f37/653796e6def70d6fb406d4b1_PICTOS-SELLSY-MAINPRODUCT-Marketing.json" data-loop="1" data-direction="1" data-autoplay="1" data-is-ix2-target="0" data-renderer="svg" data-default-duration="5" data-duration="0"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 120 120" width="120" height="120" preserveAspectRatio="xMidYMid meet" style="width: 100%; height: 100%; transform: translate3d(0px, 0px, 0px); content-visibility: visible;"><defs><clipPath id="__lottie_element_7"><rect width="120" height="120" x="0" y="0"></rect></clipPath></defs><g clip-path="url(#__lottie_element_7)"><g transform="matrix(1,0,0,1,0,0)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(1,0,0,1,60,60)"><path fill="rgb(154,166,255)" fill-opacity="1" d=" M45.310001373291016,-25.89000129699707 C45.310001373291016,-25.89000129699707 45.310001373291016,25.89000129699707 45.310001373291016,25.89000129699707 C45.310001373291016,36.6078987121582 36.6078987121582,45.310001373291016 25.89000129699707,45.310001373291016 C25.89000129699707,45.310001373291016 -25.89000129699707,45.310001373291016 -25.89000129699707,45.310001373291016 C-36.6078987121582,45.310001373291016 -45.310001373291016,36.6078987121582 -45.310001373291016,25.89000129699707 C-45.310001373291016,25.89000129699707 -45.310001373291016,-25.89000129699707 -45.310001373291016,-25.89000129699707 C-45.310001373291016,-36.6078987121582 -36.6078987121582,-45.310001373291016 -25.89000129699707,-45.310001373291016 C-25.89000129699707,-45.310001373291016 25.89000129699707,-45.310001373291016 25.89000129699707,-45.310001373291016 C36.6078987121582,-45.310001373291016 45.310001373291016,-36.6078987121582 45.310001373291016,-25.89000129699707z"></path></g></g><g transform="matrix(1,0,0,1,0,0)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(1,0,0,1,59.99999237060547,59.99000549316406)"><path stroke-linecap="round" stroke-linejoin="round" fill-opacity="0" stroke="rgb(2,1,111)" stroke-opacity="1" stroke-width="3" d=" M-4.080999851226807,-6.88100004196167 C-2.885999917984009,-7.5920000076293945 -1.4900000095367432,-8 0,-8 C0,-8 0,-8 0,-8 C4.415200233459473,-8 8,-4.415200233459473 8,0 C8,4.415200233459473 4.415200233459473,8 0,8 C-1.1039999723434448,8 -2.1559998989105225,7.776000022888184 -3.11299991607666,7.370999813079834"></path></g></g><g transform="matrix(1,0,0,1,0,0)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(1,0,0,1,59.99999237060547,59.99000549316406)"><path stroke-linecap="round" stroke-linejoin="round" fill-opacity="0" stroke="rgb(2,1,111)" stroke-opacity="1" stroke-width="3" d=" M-13.723999977111816,-10.222999572753906 C-11.069999694824219,-13.779000282287598 -7.066999912261963,-16.270999908447266 -2.4760000705718994,-16.937000274658203"></path></g></g><g transform="matrix(1,0,0,1,0,0)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(1,0,0,1,59.99999237060547,59.99000549316406)"><path stroke-linecap="round" stroke-linejoin="round" fill-opacity="0" stroke="rgb(2,1,111)" stroke-opacity="1" stroke-width="3" d="M0 0"></path></g></g></g></svg></div><div id="w-node-fca9af45-2664-e690-88f6-336e9c35756c-4b021557" class="suite-home-card-product">Marketing</div>
+                </div>
+                <h2>Generate leads and engage your customers</h2>
+                <p class="text-medium">Create superb email and SMS campaigns, automate your marketing actions, measure and optimize your performance.<br></p>
+                <div class="check-list-wrapper">
+                    <ul role="list" class="check-list">
+                        <li class="check-list-item">
+                            <img src="https://cdn.prod.website-files.com/6058a411307dccf59c654f37/60c1da7cb5dc5892dc36fdb6_green-check.svg" loading="lazy" alt="" class="check-icon">
+                            <div>Targeted email &amp; SMS campaigns</div>
+                        </li>
+                        <li class="check-list-item">
+                            <img src="https://cdn.prod.website-files.com/6058a411307dccf59c654f37/60c1da7cb5dc5892dc36fdb6_green-check.svg" loading="lazy" alt="" class="check-icon">
+                            <div>Forms, surveys &amp; landing pages</div>
+                        </li>
+                        <li class="check-list-item">
+                            <img src="https://cdn.prod.website-files.com/6058a411307dccf59c654f37/60c1da7cb5dc5892dc36fdb6_green-check.svg" loading="lazy" alt="" class="check-icon">
+                            <div>Marketing automation</div>
+                        </li>
+                        <li class="check-list-item">
+                            <img src="https://cdn.prod.website-files.com/6058a411307dccf59c654f37/60c1da7cb5dc5892dc36fdb6_green-check.svg" loading="lazy" alt="" class="check-icon">
+                            <div>A/B testing</div>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 <?php include "includes/footer.php" ?>
